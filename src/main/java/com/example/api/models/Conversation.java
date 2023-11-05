5455b4d3437ec71396a7b0f0f5ccea1a81296a5c
Rijenth <a_rijenth@laposte.net>
2023-11-05 17:36:16 +0100

    can create a conversation

--- src/main/java/com/example/api/models/Conversation.java
+++ src/main/java/com/example/api/models/Conversation.java
@@ -18,7 +18,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Entity
@@ -30,31 +32,44 @@ public class Conversation {
     private Integer id; 
 
     @Column(nullable = true)
+    @Size(min = 5, max = 50, message = "Name must be between 5 and 50 characters")
     private String name;
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private ConversationType type;
 
-    @JsonIgnoreProperties(value = "conversations")
     @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.REFRESH})
     @JoinTable(
         name="users_conversations",
         joinColumns={@JoinColumn(name="CONVERSATION_ID", referencedColumnName="ID")},
         inverseJoinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")}
     )
-    private List<User> users = new ArrayList<User>();
+    @JsonIgnoreProperties(value = "conversations")
+    private List<User> participants = new ArrayList<User>();
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)    
+    @JoinColumn(name = "conversation_id")
+    private List<Message> messages = new ArrayList<Message>();
+
+    public List<Message> messages() {
+        return messages;
+    }
 
     public List<User> participants() {
-        return users;
+        return participants;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setParticipants(List<User> users) {
-        this.users = users;
+    public void setParticipants(List<User> participants) {
+        this.participants = participants;
     }
 
     public void setType(ConversationType type) {
