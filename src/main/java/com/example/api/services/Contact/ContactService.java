009e8ace3c32bcfc1cb475f6aa994441b412cb22
JinSu77 <adrien2098@hotmail.fr>
2023-11-08 22:31:30 +0100

    Contact get random jwt

--- src/main/java/com/example/api/services/Contact/ContactService.java
+++ src/main/java/com/example/api/services/Contact/ContactService.java
@@ -2,18 +2,24 @@ package com.example.api.services.Contact;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.api.models.Contact;
+import com.example.api.models.User;
 import com.example.api.repositories.ContactRepository;
+import com.example.api.repositories.UserRepository;
+import com.example.api.services.Auth.JwtUtil;
 import com.example.api.validation.Contacts.ContactDTO;
 
 @Service
 public class ContactService {
     @Autowired
     ContactRepository contactRepository;
+    private JwtUtil jwtUtil;
+    UserRepository userRepository;
 
     public List<Contact> getAllContacts() {
         List<Contact> contacts = new ArrayList<Contact>();
@@ -23,6 +29,49 @@ public class ContactService {
         return contacts;
     }
 
+    public List<Contact> getRandomContacts(String token) {
+
+        String authenticatedUsername = jwtUtil.getAuthUsername(token);
+
+        User user = userRepository.findByUsername(authenticatedUsername);
+    
+        List<Contact> randomContacts = new ArrayList<>();
+        Random random = new Random();
+        int count = 5;
+    
+        List<Contact> allContacts = (List<Contact>) contactRepository.findAll();
+    
+        while (randomContacts.size() < count) {
+            int randomIndex = random.nextInt(allContacts.size());
+            Contact randomContact = allContacts.get(randomIndex);
+    
+            if (!randomContact.getUsername().equals(user.getUsername()) && !randomContacts.contains(randomContact)) {
+                randomContacts.add(randomContact);
+            }
+        }
+        return randomContacts;
+    }
+
+    //SANS JWT MARCHE MAIS GET L'USER
+    // public List<Contact> getRandomContacts() {
+
+    //     List<Contact> randomContacts = new ArrayList<>();
+    //     Random random = new Random();
+    //     int count = 5;
+    
+    //     List<Contact> allContacts = (List<Contact>) contactRepository.findAll();
+    
+    //     while (randomContacts.size() < count) {
+    //         int randomIndex = random.nextInt(allContacts.size());
+    //         Contact randomContact = allContacts.get(randomIndex);
+    
+    //         if (!randomContacts.contains(randomContact)) {
+    //             randomContacts.add(randomContact);
+    //         }
+    //     }
+    //     return randomContacts;
+    // }
+    
     public Contact getContactById(int id)   
     {
         return contactRepository.findById(id).get();
