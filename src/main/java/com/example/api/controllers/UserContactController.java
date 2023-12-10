0324f4eb2034c4a5c3c197d22543116baba1bcc5
Rijenth <a_rijenth@laposte.net>
2023-12-10 18:14:55 +0100

    refacto user contact relationship

--- src/main/java/com/example/api/controllers/UserContactController.java
+++ src/main/java/com/example/api/controllers/UserContactController.java
@@ -1,6 +1,7 @@
 package com.example.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,43 +14,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.api.handlers.ResponseHandler;
-import com.example.api.models.Contact;
-import com.example.api.services.Contact.ContactService;
+import com.example.api.models.User;
+import com.example.api.repositories.UserRepository;
 import com.example.api.services.User.UserService;
 
 @Controller
 @RequestMapping("/users/{userId}/contacts")
 public class UserContactController {
-    @Autowired 
-    ContactService contactService;
-
     @Autowired
     UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/random")
+    private ResponseEntity<Object> random(
+        @PathVariable("userId") Integer userId
+    ) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return ResponseHandler.generateResponse(HttpStatus.NOT_FOUND, null, "User not found");
+        }
+
+        List<User> contacts = userService.getRandomContactListFor(user.get());
+
+        return ResponseHandler.generateResponse(HttpStatus.OK, "contacts", contacts);
+    }
 
     @GetMapping
     public ResponseEntity<Object> index(
         @PathVariable("userId") Integer userId
     ) {
         try {
-            List<Contact> contacts = userService.getContactList(userId);
+            List<User> contacts = userService.getContactList(userId);
 
             return ResponseHandler.generateResponse(HttpStatus.OK, "contacts", contacts);
         } catch (ResponseStatusException responseStatusException) {
             return ResponseHandler.generateResponse(responseStatusException, null, responseStatusException.getMessage());
-        } catch (Exception e) {
-            return ResponseHandler.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, null, e.getMessage());
-        }
-    }
-
-    @GetMapping("/{contactId}")
-    public ResponseEntity<Object> show(
-        @PathVariable("userId") Integer userId,
-        @PathVariable("contactId") Integer contactId
-    ) {
-        try {
-            Contact contact = contactService.getContactById(contactId);
-
-            return ResponseHandler.generateResponse(HttpStatus.OK, "contact", contact);
         } catch (Exception e) {
             return ResponseHandler.generateResponse(HttpStatus.INTERNAL_SERVER_ERROR, null, e.getMessage());
         }
@@ -61,11 +63,9 @@ public class UserContactController {
         @PathVariable("contactId") Integer contactId
     ) {
         try {
-            userService.addContactList(userId, contactId);
+            List<User> contacts = userService.addContactList(userId, contactId);
 
-            Contact contact = contactService.getContactById(contactId);
-
-            return ResponseHandler.generateResponse(HttpStatus.OK, "contact", contact);
+            return ResponseHandler.generateResponse(HttpStatus.OK, "contacts", contacts);
         } catch (ResponseStatusException responseStatusException) {
             return ResponseHandler.generateResponse(responseStatusException, null, responseStatusException.getMessage());
         } catch (Exception e) {
--- src/main/java/com/example/api/services/User/UserService.java
+++ src/main/java/com/example/api/services/User/UserService.java
@@ -2,22 +2,23 @@ package com.example.api.services.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.api.dtos.Auth.UserLoginDTO;
 import com.example.api.dtos.Users.UserDto;
-import com.example.api.models.Contact;
 import com.example.api.models.Conversation;
 import com.example.api.models.Role;
 import com.example.api.models.User;
-import com.example.api.repositories.ContactRepository;
 import com.example.api.repositories.RoleRepository;
 import com.example.api.repositories.UserRepository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -26,10 +27,15 @@ public class UserService implements IUserService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
-    @Autowired
-    private ContactRepository contactRepository;
 
-    public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, ContactRepository contactRepository) {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public UserService(
+        UserRepository userRepository, 
+        RoleRepository roleRepository, 
+        PasswordEncoder passwordEncoder
+    ) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
@@ -77,18 +83,6 @@ public class UserService implements IUserService {
         user.setRoles(Arrays.asList(role));
 
         userRepository.save(user);
-
-        Contact contact = new Contact();
-
-        contact.setUsername(user.getUsername());
-        contact.setFirstName(user.getFirstName());
-        contact.setLastName(user.getLastName());
-        contact.setEmail(user.getEmail());
-        contact.setUserId(user.getId());
-        
-        user.setContacts(contact);
-
-        contactRepository.save(contact);
     }
 
     @Override
@@ -137,34 +131,31 @@ public class UserService implements IUserService {
         return user.get().getConversations();
     }
 
-    public Contact addContactList(Integer userId, Integer contactId) {
-            
+    public List<User> addContactList(Integer userId, Integer contactId) {
             Optional<User> optionalUser = userRepository.findById(userId);
+            Optional<User> optionalContact = userRepository.findById(contactId);
 
-            if (optionalUser.isEmpty()) {
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+            if (optionalUser.isEmpty() || optionalContact.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User or contact not found");
             }
 
             User user = optionalUser.get();
+            User contact = optionalContact.get();
 
-            Optional<Contact> optionalContact = contactRepository.findById(contactId);
-
-            if (optionalContact.isEmpty()) {
-                throw new ResponseStatusException (HttpStatus.NOT_FOUND, "Contact not found");
-            }        
-
-            Contact contact = optionalContact.get();
+            if (user.getContacts().contains(contact) || contact.getContacts().contains(user)) {
+                throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Contact already exist");
+            }
 
             user.getContacts().add(contact);
-
-            contact.getUsers().add(user);
+            contact.getContacts().add(user);
 
             userRepository.save(user);
+            userRepository.save(contact);
 
-            return contact;
+            return user.getContacts();
     }
 
-    public List<Contact> getContactList(Integer userId) {
+    public List<User> getContactList(Integer userId) {
         
             Optional<User> optionalUser = userRepository.findById(userId);
 
@@ -175,5 +166,28 @@ public class UserService implements IUserService {
             User user = optionalUser.get();
 
             return user.getContacts();
+    }
+
+    public List<User> getRandomContactListFor(User user) {
+        List<User> randomContacts = new ArrayList<User>();
+
+
+        List<String> contactIds = user.getContacts().stream().map(contact -> contact.getId().toString()).collect(Collectors.toList());
+
+        System.out.println(contactIds);
+
+        String query = "SELECT * FROM users WHERE username != '" + user.getUsername() 
+            + "' AND id NOT IN (" + String.join(",", contactIds) + ")"
+            + " ORDER BY RAND() LIMIT 5";
+
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(query);
+
+        for (Map<String, Object> row : rows) {
+            User contact = userRepository.findById((Integer) row.get("id")).get();
+
+            randomContacts.add(contact);
+        }
+
+        return randomContacts;
     }
 }
